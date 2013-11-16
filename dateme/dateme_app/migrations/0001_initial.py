aa87f04dce03c03639692dbe5f3c855ee94ec460
Nicholas Romito <nrromito@gmail.com>
2013-11-16 14:43:14 -0600

    Updated admin.py and models.py with test code.  Added migrations

---
+++ dateme/dateme_app/migrations/0001_initial.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Test'
+        db.create_table(u'dateme_app_test', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(unique=True, max_length=255)),
+        ))
+        db.send_create_signal(u'dateme_app', ['Test'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Test'
+        db.delete_table(u'dateme_app_test')
+
+
+    models = {
+        u'dateme_app.test': {
+            'Meta': {'object_name': 'Test'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'})
+        }
+    }
+
+    complete_apps = ['dateme_app']
